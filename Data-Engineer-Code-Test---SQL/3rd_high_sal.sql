af728bedc1e25621f815cad462bed40905f70818
naveen802 <naveenn.rch@gmail.com>
2026-04-28 20:29:22 +0530

    updated

--- Data-Engineer-Code-Test---SQL/3rd_high_sal.sql
+++ Data-Engineer-Code-Test---SQL/3rd_high_sal.sql
@@ -1,2 +1,3 @@
-select * from (select eid,ename,sal,dense_rank() over(partition by id) as rn from empy
-where rn = 3) empy 
+select * from
+(select eid,ename,sal,job,dense_rank() over(partition by id) as rn from empy
+where rn = 3) empy;
